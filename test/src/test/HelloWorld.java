10ca2562908e3bfc3f5b9fa724285f0d50877a6f
Ashwini <ashushree.m10@gmail.com>
2016-07-24 19:44:26 +0530

    After push, testing: 24/7 19:44

--- test/src/test/HelloWorld.java
+++ test/src/test/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("using GIT");
+		
+		System.out.println("successful pushed in git");
 	}
 
 }
